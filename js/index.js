b3f1f6fa89a1fe4c38140c01f239e042b1dfcdb2
shonb6570 <shaunbolak@gmail.com>
2021-03-10 17:38:04 -0500

    Added intersection observer animation

--- js/index.js
+++ js/index.js
@@ -20,12 +20,32 @@ function scrollFunction() {
     }  else {
         header.style.height = "150px";
         logo.style.height = "120px";
+        logo.style.marginBottom = "10px";
     
     }
     
-}  
+};  
 
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 window.onscroll = function() {scrollFunction()};
 
 
+// intersection observer - fade/bounce in animation function
+
+const bounceIn = document.querySelectorAll('.bounceIn');
+
+    observer = new IntersectionObserver((entries) => {
+
+        entries.forEach(entry => {
+        if(entry.intersectionRatio > 0) {
+            entry.target.style.animation = `bounce-in 1s ${entry.target.dataset.delay} forwards ease`;
+        } else {
+            entry.target.style.animation = "none";
+        }
+    })
+
+});
+
+bounceIn.forEach (animationElement => {
+    observer.observe(animationElement);
+});
